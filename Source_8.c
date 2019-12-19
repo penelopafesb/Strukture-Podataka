6a1cd2f3f37c063103d584223eee2802c05c79c1
penelopafesb <marin.pejcin.00@fesb.hr>
2019-12-19 23:08:00 +0100

    Vjezba 8

--- Source_8.c
+++ Source_8.c
@@ -14,22 +14,30 @@ izlaz
 
 typedef struct _stablo Stablo;
 struct _stablo {
-	char *ime;
+	char* ime;
 
 	Stablo* child;
 	Stablo* sibling;
 };
 
 typedef struct _stog Stog;
-struct _stog{
-	Stablo *directory;
+struct _stog {
+	Stablo* directory;
 
 	Stog* next;
+	Stog* prev;
 };
 
-Stablo* MakeDir();
+int MakeDir(Stablo*);
 int Menu(Stablo*, Stog*);
-int Dir(Stablo*, Stog*);
+int Dir(Stablo*);
+Stablo* cd(Stablo*, Stog*);
+Stablo* Back(Stablo*, Stog*);
+Stablo* FindByName(char*, Stablo*);
+int PushStog(Stog*, Stablo*);
+Stablo* PopStog(Stog*);
+int Linija(Stog*, Stablo*, Stablo*);
+Stog* FindLast(Stog*);
 
 int main()
 {
@@ -40,6 +48,7 @@ int main()
 	Stog* head;
 	head = (Stog*)malloc(sizeof(Stog));
 	head->next = NULL;
+	head->prev = NULL;
 	int status = 0;
 	root->ime = (char*)malloc(MAX * sizeof(char));
 	root->ime = "C:";
@@ -51,36 +60,84 @@ int main()
 	return 0;
 }
 
+int Linija(Stog* head, Stablo* root, Stablo* currentFile)
+{
+	Stog* s;
+
+	s = FindLast(head);
+
+	printf("\n");
+	if (head->next == NULL) {
+		printf("%s>", root->ime);
+		return 0;
+	}
+
+	while (s->prev!=NULL) {
+		printf("%s>", s->directory->ime);
+		s = s->prev;
+	}
+	
+	printf("%s>", currentFile->ime);
+
+	return 0;
+}
+
+Stog* FindLast(Stog* head)
+{
+	Stog* s;
+	s = head->next;
+
+	if (s == NULL) {
+		return NULL;
+	}
+
+	while (s->next != NULL) {
+		s = s->next;
+	}
+
+	return s;
+}
+
 int Menu(Stablo* root, Stog* head) {
 	Stablo* CurrentFile = root;
 	char command[MAX] = { 0 };
 
-	printf("****************************\n");
-	printf("md - Dodaj novi direktorij\n");
-	printf("cd - Otvoti direktorij\n");
+	printf("****************************************\n");
+	printf("md <filename> - Dodaj novi direktorij\n");
+	printf("cd <filename> - Otvori direktorij\n");
 	printf("cd.. - Zatvoriti direktorij\n");
 	printf("dir - Ispisi listu direktorija u trenutnome direktoriju\n");
+	printf("refresh - Ponovo ispisi izbornik\n");
 	printf("exit - Izlaz\n");
-	printf("****************************\n");
+	printf("****************************************\n");
 
 	while (1) {
-		printf("\n%s>", CurrentFile->ime);
+		//printf("%s>", CurrentFile->ime);
+		Linija(head, root, CurrentFile);
 		scanf("%s", command);
 
 		if (!strcmp(command, "md")) {
-			CurrentFile->child = MakeDir();
+			MakeDir(CurrentFile);
 		}
 
 		else if (!strcmp(command, "cd")) {
+			CurrentFile=cd(CurrentFile, head);
+		}
 
+		else if (!strcmp(command, "cd..")) {
+			CurrentFile = Back(CurrentFile, head);
 		}
 
 		else if (!strcmp(command, "dir")) {
-			Dir(CurrentFile, head);
+			Dir(CurrentFile);
 		}
 
 		else if (!strcmp(command, "exit")) {
 			return 1;
+		}
+
+		else if (!strcmp(command, "refresh")) {
+			return 0;
 		}
 
 		else
@@ -90,28 +147,142 @@ int Menu(Stablo* root, Stog* head) {
 	return 0;
 }
 
-Stablo* MakeDir(){
-	Stablo *directory;
+int MakeDir(Stablo* currentFile) {
+	Stablo* directory;
 	directory = (Stablo*)malloc(sizeof(Stablo));
-	directory->ime = (char*)malloc(MAX*sizeof(char));
+	directory->ime = (char*)malloc(MAX * sizeof(char));
 
 	if (!directory)
 		printf("Memorija nije uspjesno alocirana");
 
-	printf("Unesite ime direktorija: ");
+	if (currentFile->child != NULL) {
+		currentFile = currentFile->child;
+		while (currentFile->sibling != NULL) {
+			currentFile = currentFile->sibling;
+		}
+		currentFile->sibling=directory;
+	}
+
+	else
+		currentFile->child=directory;
+
+	//printf("Unesite ime direktorija: ");
 	scanf(" %s", directory->ime);
 
 	directory->sibling = NULL;
 	directory->child = NULL;
 
- 	return directory;
+	return 0;
 }
 
-int Dir(Stablo* currentFile, Stog* head) {
-	if (currentFile->child != NULL) {
-		printf("DIRECTORIES: ");
-		printf("\t- %s\n", currentFile->child->ime);
+int Dir(Stablo* currentFile) {
+	if (currentFile->child == NULL) {
+		printf("Prazan direktorij!\n");
+		return 0;
 	}
+		
+	currentFile = currentFile->child;
+
+	printf("DIRECTORIES: \n");
+	printf("\t- %s\n", currentFile->ime);
+	
+	while (currentFile->sibling != NULL) {
+		printf("\t- %s\n", currentFile->sibling->ime);
+		currentFile = currentFile->sibling;
+	}	
 
 	return 0;
+}
+
+Stablo* FindByName(char* name, Stablo* currentFile) {
+	if (currentFile->child == NULL) {
+		printf("Direktorij je prazan!");
+		return 0;
+	}
+
+	currentFile = currentFile->child;
+	
+	while (strcmp(name, currentFile->ime) != 0 && currentFile->sibling !=NULL) {
+		currentFile = currentFile->sibling;
+	}
+
+	if (currentFile->sibling == NULL) {
+		if (strcmp(name, currentFile->ime) != 0)
+			return 0;
+		else
+			return currentFile;
+	}
+
+	else
+		return currentFile;
+}
+
+int PushStog(Stog* head, Stablo* directory) {
+	Stog* q = (Stog*)malloc(sizeof(Stog));
+	
+	q->next = head->next;
+	q->prev = head;
+	if (head->next != NULL) {
+		head->next->prev = q;
+	}
+	head->next = q;
+	q->directory = directory;
+
+	 return 0;
+}
+
+Stablo* PopStog(Stog* head) {
+	Stog* q = (Stog*)malloc(sizeof(Stog));
+	Stablo* p;
+
+	if (head->next == NULL)
+		return NULL;
+
+	q = head->next;
+	p = q->directory;
+	head->next = head->next->next;
+	if(head->next!=NULL)
+		head->next->prev = head;
+	free(q);
+
+	return p;
+}
+
+Stablo* Back(Stablo* currentFile, Stog* head)
+{
+	Stablo* SearchedFile;
+	
+	SearchedFile = PopStog(head);
+
+	if (SearchedFile == NULL) {
+		printf("Ne moze se izaci iz root direktorija!\n");
+		SearchedFile = currentFile;
+	}
+	
+	return SearchedFile;
+}
+
+Stablo* cd(Stablo* currentFile, Stog* head)
+{
+	Stablo* SearchedFile;
+	char* imeDir;
+	imeDir = (char*)malloc(MAX * sizeof(char));
+	
+	//printf("Koji direktorij zelite otvoriti: ");
+	scanf("%s", imeDir);
+
+	if (currentFile->child == NULL) {
+		printf("Nema direktorija koji mozete otvoriti!\n");
+		return currentFile;
+	}
+	
+	SearchedFile=FindByName(imeDir, currentFile);
+	if (SearchedFile == 0) {
+		printf("Direktorij nije pronaden\n");
+		return currentFile;
+	}
+
+	PushStog(head, currentFile);
+
+	return SearchedFile;
 }
